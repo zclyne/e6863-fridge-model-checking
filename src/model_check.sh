3817301191725c24f228b28355c07f6fd83d40d9
qq544259335 <544259335@qq.com>
2022-11-30 10:48:50 -0800

    add assertion generator properties

--- src/model_check.sh
+++ src/model_check.sh
@@ -1,7 +1,7 @@
-cbmc fridge.c --unwind 18 --function kkv_init --trace
-cbmc fridge.c --unwind 18 --function kkv_destroy --trace
-cbmc fridge.c --unwind 18 --function kkv_put --trace
-cbmc fridge.c --unwind 18 --function kkv_get --trace
-cbmc main.c --unwind 18 --function non_blocking_get --trace
-cbmc main.c --unwind 18 --function blocking_get --trace
+cbmc fridge.c --unwind 18 --function kkv_init --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check 
+cbmc fridge.c --unwind 18 --function kkv_destroy --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
+cbmc fridge.c --unwind 18 --function kkv_put --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
+cbmc fridge.c --unwind 18 --function kkv_get --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
+cbmc main.c --unwind 18 --function non_blocking_get --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
+cbmc main.c --unwind 18 --function blocking_get --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
 
