e23be336e4076ad8dbe81e92cc58ccd930a11cd0
Yifan Zhang <zyfinori@gmail.com>
2022-12-19 15:14:28 -0500

    added program_3

--- src/model_check.sh
+++
@@ -1,7 +0,0 @@
-cbmc fridge.c --unwind 18 --function kkv_init --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check 
-cbmc fridge.c --unwind 18 --function kkv_destroy --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
-cbmc fridge.c --unwind 18 --function kkv_put --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
-cbmc fridge.c --unwind 18 --function kkv_get --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
-cbmc main.c --unwind 18 --function non_blocking_get --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
-cbmc main.c --unwind 18 --function blocking_get --trace --bounds-check --pointer-check --memory-leak-check --div-by-zero-check --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check --conversion-check
-
--- src/model_check_program_3.c
+++ src/model_check_program_3.c
@@ -8,14 +8,20 @@
 #include "fridge.h"
 #include "fridge.c"
 
-// verify kkv_put and kkv_get returns -EPERM if they are called when the store is not initialized yet
+// verify multiple kkv_puts with same key. only the last one takes effect
 int main()
 {
-    int v = 3, get_v;
+    int v1 = 1, v2 = 2, v3 = 3, get_v;
     long ret;
 
     kkv_init();
-    ret = kkv_put(1, (void *)&v, sizeof(int));
+    ret = kkv_put(1, (void *)&v1, sizeof(int));
     __CPROVER_assert(ret == 0, "kkv_put successfully puts value");
+    ret = kkv_put(1, (void *)&v2, sizeof(int));
+    __CPROVER_assert(ret == 0, "kkv_put successfully puts value");
+    ret = kkv_put(1, (void *)&v3, sizeof(int));
+    __CPROVER_assert(ret == 0, "kkv_put successfully puts value");
+    ret = kkv_get(1, (void *)&get_v, sizeof(int), 0);
+    __CPROVER_assert(ret == 0 && get_v == 3, "kkv_get successfully gets the value put by the last kkv_put");
     kkv_destroy();
 }
